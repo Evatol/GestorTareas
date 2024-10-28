2cf52512f1d481dec7d64bc3540dd3df181cce52
Evatol <148373329+Evatol@users.noreply.github.com>
2024-10-28 11:09:27 -0300

    Create TareaItem.vue

---
+++ src/components/TareaItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card p-2 mb-1">
+    <div class="d-flex justify-content-between">
+      <span>{{ tarea.title }}</span>
+      <i :class="['bi', tarea.completed ? 'bi-check-circle-fill' : 'bi-circle']"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tarea']
+};
+</script>
