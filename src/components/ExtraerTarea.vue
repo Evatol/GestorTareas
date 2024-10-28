89b9063b6d14a4eab5b19498cf24df3c31aee172
Evatol <148373329+Evatol@users.noreply.github.com>
2024-10-28 11:09:04 -0300

    Create ExtraerTarea.vue

---
+++ src/components/ExtraerTarea.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card p-3 mb-3">
+    <h4>Extraer Tareas de la API</h4>
+    <button @click="obtenerTareas" class="btn btn-secondary">Obtener Tareas</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  methods: {
+    async obtenerTareas() {
+      try {
+        const response = await axios.get('https://dummyjson.com/todos');
+        this.$emit('agregar-tareas-api', response.data.todos);
+      } catch (error) {
+        console.error("Error al obtener tareas:", error);
+      }
+    }
+  }
+};
+</script>
