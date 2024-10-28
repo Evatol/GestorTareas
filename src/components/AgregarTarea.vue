8df3ae86439bb0d21c50eca185d6903709e0bd97
Evatol <148373329+Evatol@users.noreply.github.com>
2024-10-28 11:08:41 -0300

    Create AgregarTarea.vue

---
+++ src/components/AgregarTarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card p-3 mb-3">
+    <h4>Agregar Tarea</h4>
+    <div class="input-group mb-3">
+      <input type="text" v-model="nuevaTarea" placeholder="Escribe una tarea" class="form-control">
+      <button @click="agregarTarea" class="btn btn-primary">Agregar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nuevaTarea: ''
+    };
+  },
+  methods: {
+    agregarTarea() {
+      if (this.nuevaTarea.trim()) {
+        this.$emit('nueva-tarea', { title: this.nuevaTarea, completed: false });
+        this.nuevaTarea = '';
+      }
+    }
+  }
+};
+</sc
