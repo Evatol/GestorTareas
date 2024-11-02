9b81d68a4afac31152a35313fc83f7bde775efed
Evatol <148373329+Evatol@users.noreply.github.com>
2024-11-01 21:44:19 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+  router,                 // Configura el enrutador si tienes configurado Vue Router.
+  render: h => h(App)     // Renderiza el componente App en el div con id="app".
+}).$mount('#app');        // Monta la instancia de Vue en el div con id="app".
